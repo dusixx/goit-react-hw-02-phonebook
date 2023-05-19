e402589b994be4a0e7c07452cc3d34572fa83fd0
dusixx <113414935+dusixx@users.noreply.github.com>
2023-05-19 08:17:48 +0300

    small fix

--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -2,12 +2,7 @@ export * from './notify';
 
 export const isStr = v => typeof v === 'string';
 export const isNum = v => !isNaN(v - parseFloat(v));
-export const isNonEmptyArray = v => Array.isArray(v) && v.length;
-
-export function cap(v) {
-  return isStr(v) && v ? v[0].toUpperCase() + v.slice(1) : '';
-}
-
+export const cap = v => (isStr(v) && v ? v[0].toUpperCase() + v.slice(1) : '');
 export const calcCSSValue = v => (isNum(v) ? `${v}px` : v);
 
 export const parseCSSValue = v => {
@@ -15,21 +10,6 @@ export const parseCSSValue = v => {
   const unit = String(v).slice(String(value).length) || 'px';
   return { value, unit };
 };
-
-export const getRandomHexColor = () =>
-  `#${Math.floor(Math.random() * 0xffffff)
-    .toString(0x10)
-    .padStart(6, 0)}`;
-
-export function formatNumber(v) {
-  if (!Number.isInteger(v)) return '';
-
-  const digits = String(v).length;
-  if (digits >= 7) return `${(v / 10 ** 6).toFixed(1)}M`;
-  if (digits >= 4) return `${(v / 10 ** 3).toFixed(1)}K`;
-
-  return String(v);
-}
 
 let id = 0;
 export const getId = () => `id-${(id++).toString(16)}`;
