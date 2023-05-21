b6b3da3c2ad8a0faf0fdf282addf2c20ea3443fc
dusixx <113414935+dusixx@users.noreply.github.com>
2023-05-21 07:57:16 +0300

    small fix

--- src/components/TextField/params.jsx
+++ src/components/TextField/params.jsx
@@ -9,22 +9,22 @@ export const def = {
   paddingSide: 12,
 };
 
-// inputHeight и iconWidth - вычисляемые после рендера значения
-// всегда приходят в px
+// inputHeight и iconWidth - вычисляемые и всегда приходят в px
 
 export const calc = {
   fieldWidth: ({ width }) => calcCSSValue(width) || def.width,
   fieldHeight: ({ height }) => calcCSSValue(height) || def.height,
+  borderRadius: ({ inputHeight }) => `${inputHeight * 0.2}px`,
+
   fontSize: ({ inputHeight }) => `${inputHeight * 0.35}px`,
   validationFontSize: ({ inputHeight }) => `${inputHeight * 0.35 * 0.83}px`,
-  borderRadius: ({ inputHeight }) => `${inputHeight * 0.2}px`,
+
   iconHeight: ({ size }) => `${size ? calcCSSValue(size) : '50%'}`,
   iconOffset: ({ iconWidth }) => `${iconWidth * 0.5}px`,
   clearBtnOffset: ({ inputHeight }) => `${inputHeight * 0.2}px`,
 
   paddingLeft: ({ iconWidth }) =>
     `${iconWidth ? iconWidth + iconWidth * 0.5 + 5 : def.paddingSide}px`,
-
   paddingRight: ({ inputHeight }) =>
     `${inputHeight ? inputHeight * 0.8 : def.paddingSide}px`,
 };
